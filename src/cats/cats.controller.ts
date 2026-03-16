d6e63d64922571826a39502f337a0c78920c14d6
seriiserii825 <seriiburduja@gmail.com>
2026-03-16 07:30:48 +0200

    feat: cats control no store

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Header, HttpCode, Post } from '@nestjs/common';
 
 @Controller('cats')
 export class CatsController {
   @Get()
   findAll() {
     return 'this action find all cats';
+  }
+
+  @Post()
+  @HttpCode(204)
+  @Header('Cache-Control', 'no-store')
+  create() {
+    return 'this action create cat';
   }
 }
