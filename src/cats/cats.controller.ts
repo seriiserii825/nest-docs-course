a02d90776bf7ba38ab1652d1f46ca422197fd546
seriiserii825 <seriiburduja@gmail.com>
2026-03-16 07:44:43 +0200

    feat: cats create dto

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,16 +1,24 @@
-import { Controller, Get, Header, HttpCode, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { CreateCatDto } from './dto/create-cat.dto';
 
 @Controller('cats')
 export class CatsController {
   @Get()
-  findAll() {
-    return 'this action find all cats';
+  findAll(@Query('age') age: number, @Query('breed') breed: string) {
+    return `this action find all cats with ${age} and ${breed}`;
   }
 
   @Post()
   @HttpCode(204)
-  @Header('Cache-Control', 'no-store')
-  create() {
+  create(@Body() createCatDto: CreateCatDto) {
     return 'this action create cat';
   }
 
