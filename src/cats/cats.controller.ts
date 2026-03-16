18aa23e132564fc0f15b5826263d13d49af4e399
seriiserii825 <seriiburduja@gmail.com>
2026-03-16 07:52:44 +0200

    feat: cats crud

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,13 +1,16 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   HttpCode,
   Param,
   Post,
+  Put,
   Query,
 } from '@nestjs/common';
-import { CreateCatDto } from './dto/create-cat.dto';
+import { CreateCatDto } from './dto/create-cat.dto_1';
+import { UpdateCatDto } from './dto/update-cat.dto';
 
 @Controller('cats')
 export class CatsController {
@@ -19,11 +22,22 @@ export class CatsController {
   @Post()
   @HttpCode(204)
   create(@Body() createCatDto: CreateCatDto) {
-    return 'this action create cat';
+    return `this action create cat with age: ${createCatDto.age}`;
+  }
+
+  @Put()
+  @HttpCode(204)
+  update(@Body() updateCatDto: UpdateCatDto) {
+    return `this action create cat with breed: ${updateCatDto.breed}`;
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
     return `Cat ${id}`;
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return `this action will delete cat with id: ${id}`;
   }
 }
